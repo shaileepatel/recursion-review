5e6badd192d5b07f19e0f0bc8ce6fcaa202adcfd
Derek Velzy <dmvelzy@gmail.com>
2020-10-05 15:43:03 -0700

    Fill out the format and structure of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  // Iterate through object
+  var toString = function (item){
+    if (typeof item === 'number') {
+      result += item + ',';
+    } else if (typeof item === 'string') {
+      result += '"' + item + '"' + ',';
+    } else if (typeof item === 'object' && !Array.isArray(item)) {
+      result += '{';
+      for (var key in item) {
+        toString(item[key]);
+      }
+    }
+    // If array
+  };
+  // If string/number/boolean
+
+  if (Array.isArray(obj)) {
+    result = '[';
+    for (let i = 0; i < obj.length; i++) {
+      result += toString(obj[i]) + ', ';
+    }
+    return result + ']';
+  } else if (typeof obj === 'object' && !Array.isArray(obj)) {
+    result = '{';
+    for (let key in input) {
+
+    }
+    return result + '}';
+  }
 };
