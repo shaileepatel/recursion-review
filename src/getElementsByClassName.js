9770cd884f21cfbe423db275a40989b8ec79ef37
Derek Velzy <dmvelzy@gmail.com>
2020-10-05 21:24:20 -0700

    Recursively solve getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  // Define the NodeList object of the entire body
+  let allChildren = document.body.childNodes;
+  let result = [];
+
+  var nested = function(nodes) {
+    // traverse through each element in the allChildren
+    for (let key in nodes) {
+      // If we wind up on the length property, we're done iterating through that particular NodeList
+      if (typeof nodes[key] === 'number') {
+        break;
+      }
+      // If the NodeList has a class list property, AND the class list includes the target className
+      if (nodes[key].classList !== undefined) {
+        if (nodes[key].classList.value.includes(className)) {
+          // push that element to the result array
+          result.push(nodes[key]);
+        }
+      }
+      // if the NodeList has child nodes (is nested)
+      if (nodes[key].childNodes.length > 0) {
+        // Use recursion to traverse through the nested NodeList
+        nested(nodes[key].childNodes);
+      }
+    }
+  };
+  // Call the 'nested' helper function
+  nested(allChildren);
+  // Add the body element to the result
+  result.unshift(document.body);
+  return result;
 };
