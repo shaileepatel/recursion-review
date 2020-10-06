a520ee34642fa6204a0a43e05304e9f0d7c88ae6
Derek Velzy <dmvelzy@gmail.com>
2020-10-05 17:15:28 -0700

    Get further through stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,37 +1,52 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
-
-var stringifyJSON = function(obj) {
-  var result = '';
+// but you don't so you're going to write it from scratch:\""
+var stringifyJSON = function (obj) {
+  var stringifyResult = '';
   // Iterate through object
-  var toString = function (item){
+  var toString = function (item) {
     if (typeof item === 'number') {
-      result += item + ',';
+      stringifyResult += item;
     } else if (typeof item === 'string') {
-      result += '"' + item + '"' + ',';
-    } else if (typeof item === 'object' && !Array.isArray(item)) {
-      result += '{';
+      stringifyResult += '\"'+ item + '\"';
+    } else if (typeof item === 'object' && !Array.isArray(item) && item !== null) {
+      stringifyResult += '{';
+
       for (var key in item) {
+        stringifyResult += '\"' + key + '\"' + ':';
         toString(item[key]);
+        stringifyResult += ',';
       }
+      stringifyResult = stringifyResult.substring(0, stringifyResult.length - 1) + '}';
+
+    } else if (Array.isArray(item)) {
+      stringifyResult += '[';
+      if (item.length > 0) {
+        for (let i = 0; i < item.length; i++) {
+          toString(item[i]);
+          stringifyResult += ',';
+        }
+        stringifyResult = stringifyResult.substring(0, stringifyResult.length - 1) + ']';
+      } else {
+        stringifyResult += ']';
+      }
+    } else {
+      stringifyResult += item;
     }
-    // If array
   };
-  // If string/number/boolean
+  toString(obj);
+  return stringifyResult;
 
-  if (Array.isArray(obj)) {
-    result = '[';
-    for (let i = 0; i < obj.length; i++) {
-      result += toString(obj[i]) + ', ';
+  /*
+  obj = {
+    a : 10,
+    b : [3,5,7],
+    c : "hello",
+    d : {
+      f : 9,
+      g : [4],
     }
-    return result + ']';
-  } else if (typeof obj === 'object' && !Array.isArray(obj)) {
-    result = '{';
-    for (let key in input) {
+  } */
 
-    }
-    return result + '}';
-  }
 };
